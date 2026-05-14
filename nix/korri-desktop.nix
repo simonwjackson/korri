2874c4abb6c5004c6d4d40a9dff68ae32083d01b
Simon W. Jackson <simon@simonwjackson.io>
2026-05-13 22:26:11 -0600

    feat(desktop): add deviceBinaryAliases parameter for wrapper aliases
    - Add deviceBinaryAliases parameter (default: empty list)
    - Generate wrapper aliases for device binaries with same device runtime configuration
    - Allows korri-desktop-odin and other aliases to exist alongside korri-desktop-device

--- nix/korri-desktop.nix
+++ nix/korri-desktop.nix
@@ -13,6 +13,7 @@
     pkgs.gtk3
   ],
   deviceDesktopDataDirs ? desktopDataDirs,
+  deviceBinaryAliases ? [ ],
   gioExtraModules ? pkgs.glib-networking,
   deviceGioExtraModules ? gioExtraModules,
 }:
@@ -206,6 +207,9 @@ pkgs.stdenv.mkDerivation {
 
         write_wrapper "$out/bin/korri-desktop" x11 "" "${runtimeLibraryPath}" "${desktopDataPath}" "${gioExtraModulesPath}"
         write_wrapper "$out/bin/korri-desktop-device" "" device "${deviceRuntimeLibraryPath}" "${deviceDesktopDataPath}" "${deviceGioExtraModulesPath}"
+        for alias in ${lib.escapeShellArgs deviceBinaryAliases}; do
+          write_wrapper "$out/bin/$alias" "" device "${deviceRuntimeLibraryPath}" "${deviceDesktopDataPath}" "${deviceGioExtraModulesPath}"
+        done
 
         runHook postInstall
   '';
